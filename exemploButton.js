a2cba113a5ae7dbe7bb8977a846f84feb431aa30
Nataniel Paiva <nataniel.paiva@gmail.com>
2024-12-05 21:13:08 -0300

    Exemplo de button

---
+++ exemploButton.js
@@ -0,0 +1,8 @@
+import { Button } from 'react-native';
+
+const App = () => (
+  <Button
+    title="Login"
+    onPress={() => alert('Botão clicado!')} // Aqui é a ação quando o botão é clicado
+  />
+);
