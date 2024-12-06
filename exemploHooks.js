c924101e54c9db4a327116cccfe0f99367aaaf4e
Nataniel Paiva <nataniel.paiva@gmail.com>
2024-12-06 20:52:42 -0300

    Exemplo de hooks

---
+++ exemploHooks.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text } from 'react-native';
+
+const MeuComponente = () => {
+  return <Text>Olá, eu sou um componente funcional!</Text>;
+};
+
+export default MeuComponente;
