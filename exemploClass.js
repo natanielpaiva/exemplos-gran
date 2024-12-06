c93fc817dab9f1ab57c12a89f0383a7c7bfa20d9
Nataniel Paiva <nataniel.paiva@gmail.com>
2024-12-06 20:57:01 -0300

    Exemplo de classe

---
+++ exemploClass.js
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+import { Text } from 'react-native';
+
+class MeuComponente extends Component {
+  render() {
+    return <Text>Olá, eu sou um componente de classe!</Text>;
+  }
+}
+
+export default MeuComponente;
